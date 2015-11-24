a8368badbfb3ea2fc48427588b19b9f312c66da5
fishinui <info@yasuyukiinui.com>
2015-11-24 03:34:02 +0000

    update look& feel and add validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :followings, :followers]
+  before_action :logged_in_user, only: [:edit]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @microposts = @user.microposts
@@ -26,6 +28,7 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+    
   end
   
   def create
@@ -48,5 +51,18 @@ private
   def set_user
     @user = User.find(params[:id])
   end
+  
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+  
 end
   
